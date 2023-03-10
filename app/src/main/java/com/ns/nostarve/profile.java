7e5eb5643c2a31a2ec2375ac3785ddf9cf9c50a1
ansh verma <anshv309@gmail.com>
2023-03-10 23:07:08 +0530

    hii

---
+++ app/src/main/java/com/ns/nostarve/profile.java
@@ -0,0 +1,15 @@
+package com.ns.nostarve;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().setTitle("NoStarve");
+    }
+}
